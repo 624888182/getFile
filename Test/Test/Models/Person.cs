06a231cd28c2f57ffa138c8814e0eab4bbf01a96
624888182 <18033668578@163.com>
2017-06-15 12:01:00 +0800

    提交

---
+++ Test/Test/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class Person
+    {
+        public string id { set; get; }
+        public string name { set; get; }
+        public string age { set; get; }
+    }
+}
